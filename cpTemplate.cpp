6aba3dadb27de93258c586fd0985fbe5173cd185
KALLEM SHIVASHEKARREDDY <54935302+kallemshivashekarreddy@users.noreply.github.com>
2020-06-08 23:19:08 +0530

    Update cpTemplate.cpp

--- cpTemplate.cpp
+++ cpTemplate.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #define ll long long int
-#define for(i,n)
+#define fo(i,n)
 #define pb push_back
 #define mp make_pair
 #define F first
@@ -10,12 +10,12 @@
 #define sortall(x) sort(all(x))
 #define tr(it,a) for(auto it=a.begin();it!=a.end();it++)
 #define vvii(a,n,m) vector<vector<int>> a(n,vector<int>(m,0))
-typedef pair<int,int> pii;
-typedef vector<int> vi;
-typedef vector<ll>  vl;
-typedef vector<pii> vpii;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
+#define pii pair<int,int> 
+#define vi vector<int>
+#define vl vector<ll>  
+#define vpii vector<pii> 
+#define vvi vector<vi> 
+#define vvl vector<vl> 
 using namespace std;
 int power(int base, int exp); 
 const int mod = 1000000007;
@@ -28,16 +28,17 @@ int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
 
-    int t = 1;
+    int t;
     cin >> t;
     while(t--) {
+
       solve();
     }
 
     return 0;
 }
 
-int mpow(int base, int exp) {
+int power(int base, int exp) {
   base %= mod;
   int result = 1;
   while (exp > 0) {
